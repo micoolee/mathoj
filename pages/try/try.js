0ef091bf6480956f7c21e35967e464c8ad3f574f
mikelee1 <liyuanye123456@gmail.com>
2018-04-12 22:49:37 +0800

    websocket version 0.0

--- pages/try/try.js
+++ pages/try/try.js
@@ -8,22 +8,73 @@ Page({
   data: {
   
   },
+  connect:function(){
+console.log('aaa')
+  },
 send:function(){
 
+
+
+  var socketOpen = false
+  var socketMsgQueue = ['a','b']
   wx.connectSocket({
-    url: 'ws://www.liyuanye.club/'
+    url: 'ws://www.liyuanye.club/testwss/'
   })
+
+  wx.onSocketOpen(function (res) {
+    socketOpen = true
+    for (var i = 0; i < socketMsgQueue.length; i++) {
+      sendSocketMessage(socketMsgQueue[i])
+    }
+    socketMsgQueue = []
+  })
+
 
   wx.onSocketMessage(function (res) {
     console.log('收到服务器内容：' + res.data)
   })
 
 
+  function sendSocketMessage(msg) {
+    if (socketOpen) {
+      wx.sendSocketMessage({
+        data: msg,
+        success:function(res){
+          console.log('wh')
+          console.log(res)
+        }
+      })
+    } else {
+      socketMsgQueue.push(msg)
+    }
+  }
+
 
 },
   /**
    * 生命周期函数--监听页面加载
    */
+
+
+close:function(){
+
+
+  wx.closeSocket({
+
+  })
+
+  wx.onSocketClose(function (res) {
+    console.log('WebSocket 已关闭！')
+  })
+
+
+
+
+},
+
+
+
+
   onLoad: function (options) {
   
   },
