55195d1bfaaaf389834fdfbe135cf7be66210717
mike <liyy@yunphant.com>
2018-10-13 13:20:02 +0800

    update

--- pages/getuserinfo/getuserinfo.js
+++ pages/getuserinfo/getuserinfo.js
@@ -2,33 +2,36 @@
 const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    hidewarning:true
+  },
+  bindGetUserInfo: function (e) {
+    console.log(e)
+    if(e.detail.userInfo){
+      app.globalData.nickname = e.detail.userInfo.nickName
+      app.globalData.avatar = e.detail.userInfo.avatarUrl
+      wx.request({
+        url: app.globalData.baseurl + '/uploadavatar/',
+        method: 'post',
+        data: { 'userid': app.globalData.openid, 'username': app.globalData.nickname, 'avatar': app.globalData.avatar },
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+      })
+      wx.navigateBack({
+      })
+    }
+  },
 
-    data: {
-      canIUse: wx.canIUse('button.open-type.getUserInfo'),
-      hidewarning:true
-    },
-    bindGetUserInfo: function (e) {
-      if(e.detail.userInfo){
-        app.globalData.nickname = e.detail.userInfo.nickName
-        app.globalData.avatar = e.detail.userInfo.avatarUrl
-        wx.request({
-          url: app.globalData.baseurl + '/uploadavatar/',
-          method: 'post',
-          data: { 'userid': app.globalData.openid, 'username': app.globalData.nickname, 'avatar': app.globalData.avatar },
-          header: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-        })
+  nobindGetUserInfo: function (e) {
+    console.log(e)
+    console.log(app)
+    wx.navigateBack({
+    })
+  },
 
 
-
-        wx.navigateBack({
-        })
-      }
-    },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -41,52 +44,5 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
