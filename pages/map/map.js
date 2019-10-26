2be70af8ec77849bfb0625cbf445fcb86b21a0fc
mike <liyy@yunphant.com>
2019-10-26 16:12:21 +0800

    修改map为可展示学生信息

--- pages/map/map.js
+++ pages/map/map.js
@@ -5,9 +5,11 @@ var console = require('../../utils/console.js')
 const app = getApp()
 var userlatitude = 0
 var userlongitude = 0
+var teacherorstudent = 'teacher'
 
 Page({
   data: {
+    mapheightratio: 1,
     latitude: 0,
     longitude: 0,
     height: app.globalData.screenheight,
@@ -66,7 +68,9 @@ Page({
       clickable: true
     }
     ],
-    mapCtx: null
+    mapCtx: null,
+    profile: '',
+    problems: []
   },
   onReady: function (e) {
     // 使用 wx.createMapContext 获取 map 上下文
@@ -99,8 +103,36 @@ Page({
   regionchange(e) {
     console.log(e)
   },
+  mapclick(e) {
+    this.setData({
+      mapheightratio: 1
+    })
+  },
   markertap(e) {
-    console.log(e)
+    var that = this
+    if (teacherorstudent == 'teacher') {
+      network.post('/problem/getonenearbyprincipal', {
+        'userid': app.globalData.selfuserid,
+        'principalid': e.markerId
+      }, function (res) {
+        that.setData({
+          profile: res.profile || {},
+          mapheightratio: 0.3
+        })
+      })
+    } else {
+      network.post('/problem/getstudentproblem', {
+        'userid': app.globalData.selfuserid,
+        'studentid': e.markerId
+      }, function (res) {
+        that.setData({
+          profile: res.profile || {},
+          problems: res.problems || [],
+          mapheightratio: 0.3
+        })
+      })
+    }
+
   },
 
   controltap(e) {
@@ -128,9 +160,11 @@ Page({
       })
     } else if (e.controlId === 4) {
       //teacher
+      teacherorstudent = 'teacher'
       that.formsubmitteacher()
     } else if (e.controlId === 5) {
       //student
+      teacherorstudent = 'student'
       that.formsubmitstudent()
     }
 
@@ -144,6 +178,7 @@ Page({
       let markers = [];
       if (res.location) {
         for (let item of res.location) {
+          console.log('item: ', item)
           let marker = that.createMarker(item);
           markers.push(marker)
         }
@@ -165,11 +200,11 @@ Page({
       }
     })
   },
-
+  // 根据point创建标记点
   createMarker(point) {
     let marker = {
       iconPath: point.avatar,
-      id: point.id || 0,
+      id: point.userid || 0,
       name: point.username || '',
       latitude: point.latitude,
       longitude: point.longitude,
